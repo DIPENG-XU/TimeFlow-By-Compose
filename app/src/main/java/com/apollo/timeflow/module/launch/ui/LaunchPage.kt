fe87851ffaf5047ed189f378e573564d4a0fcb3e
apollo <dipengxu@163.com>
2024-06-25 20:32:56 +0800

    Finish Version 2.2.4
    1. Add a Launch Page

---
+++ app/src/main/java/com/apollo/timeflow/module/launch/ui/LaunchPage.kt
@@ -0,0 +1,64 @@
+package com.apollo.timeflow.module.launch.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelStoreOwner
+import androidx.navigation.NavHostController
+import com.apollo.timeflow.R
+import com.apollo.timeflow.module.launch.viewmodel.LaunchViewModel
+import com.apollo.timeflow.module.moduleNavHost.NavHostRouteConfig
+import com.apollo.timeflow.utils.getFontSizeInHomeFeed
+import com.apollo.timeflow.viewmodel.TimeViewModel
+import kotlinx.coroutines.delay
+
+@Composable
+fun LaunchPage(
+    viewModelStoreOwner: ViewModelStoreOwner,
+    navController: NavHostController,
+) {
+    val timeViewModel = hiltViewModel<TimeViewModel>(viewModelStoreOwner)
+    val deviceType = timeViewModel.deviceUIState.value
+
+    val launchViewModel = hiltViewModel<LaunchViewModel>(viewModelStoreOwner)
+
+    Box {
+        Text(
+            launchViewModel.slogan.value,
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.primary,
+            lineHeight = 48.sp,
+            fontFamily = FontFamily(
+                fonts = listOf(
+                    Font(R.font.poppins_bold, FontWeight.Light),
+                )
+            ),
+            fontSize = getFontSizeInHomeFeed(deviceType),
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(12.dp),
+        )
+    }
+
+    launchViewModel.fetchWelcomeSlogan()
+
+    LaunchedEffect(key1 = Lifecycle.State.CREATED) {
+        delay(5000L)
+        navController.popBackStack()
+        navController.navigate(NavHostRouteConfig.NAV_HOST_ROUTE_FOR_HOMEFEED)
+    }
+}
+
---
+++ app/src/main/java/com/apollo/timeflow/module/launch/viewmodel/LaunchViewModel.kt
@@ -0,0 +1,51 @@
+package com.apollo.timeflow.module.launch.viewmodel
+
+import android.app.Application
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.apollo.timeflow.R
+import com.apollo.timeflow.module.launch.service.feature.ILaunchService
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
+
+@HiltViewModel
+class LaunchViewModel @Inject constructor(
+    private val _coroutine: CoroutineContext,
+    private val _iLaunchService: ILaunchService,
+    private val _application: Application,
+): AndroidViewModel(_application) {
+
+
+    private val _slogan: MutableState<String> = mutableStateOf("")
+    val slogan: State<String> = _slogan
+
+    fun fetchWelcomeSlogan() = viewModelScope.launch(_coroutine) {
+        val timeStage = async {
+            _iLaunchService.fetchTimeStage()
+        }
+        val dayOfWeek = async {
+            _iLaunchService.fetchDayOfWeek()
+        }
+        withContext(Dispatchers.Main) {
+            _slogan.value = String.format(
+                _application.getString(
+                    R.string.welcome_in_launch_page
+                ),
+                _application.getString(
+                    dayOfWeek.await().stringResource,
+                ),
+                _application.getString(
+                    timeStage.await().stringResource,
+                )
+            )
+        }
+    }
+}
